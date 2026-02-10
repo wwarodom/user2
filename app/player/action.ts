4d4b48c4030cae6f526885808c5bb6ae55699f74
Warodom Werapun <wwarodom@gmail.com>
2026-02-10 10:26:35 +0700

    add useActionState

---
+++ app/player/action.ts
@@ -0,0 +1,8 @@
+export default function submitPlayer() {
+
+
+    return {
+        error: {},
+        data: {}
+    }
+}
--- app/player/page.tsx
+++ app/player/page.tsx
@@ -1,15 +1,22 @@
+'use client'
+import { useActionState } from "react"
+import submitPlayer from "./action"
 export default function PlayerPage() {
-
     const initState = {
-        name: "",
-        age: 0,
-        email: ""
+        error: {},
+        data: {
+            name: "",
+            age: 0,
+            email: ""
+        }
     }
+
+    const [state, action] = useActionState(submitPlayer, initState)
 
     return (
         <div className="flex flex-col justify-center items-center h-screen">
             <h1>Player Page</h1>
-            <form>
+            <form action={action}>
                 <div>
                     Name: <input className="border my-2 px-2"
                         type="text" name="name" />
